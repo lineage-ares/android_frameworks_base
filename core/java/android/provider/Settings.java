8ade184ee408007c0dd184e89868f688b1ac6bc8
cjybyjk <cjybyjk@zjnu.edu.cn>
2022-09-05 04:14:52 +0000

    base: support per-app volume [2/3]
    Signed-off-by: cjybyjk <cjybyjk@zjnu.edu.cn>
    Change-Id: Iaa97787d3820b4f6c23fda678ec3ffe711061c4f
    
    Change-Id: I469582d9b43989c2db45ae1e992b59707b14d642
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Tkpointz <pointzztk@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5564,6 +5564,12 @@ public final class Settings {
         public static final String THREE_FINGER_GESTURE = "three_finger_gesture";
 
         /**
+         * Show app volume rows in volume panel
+         * @hide
+         */
+        public static final String SHOW_APP_VOLUME = "show_app_volume";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -18345,6 +18351,12 @@ public final class Settings {
         @SdkConstant(SdkConstant.SdkConstantType.ACTIVITY_INTENT_ACTION)
         public static final String ACTION_VOLUME =
                 "android.settings.panel.action.VOLUME";
+
+        /**
+         * @hide
+         */
+        public static final String ACTION_APP_VOLUME =
+                "android.settings.panel.action.APP_VOLUME";
     }
 
     /**
